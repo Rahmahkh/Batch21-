6b606626352f495988ecb539cd907eb9d24ee5a7
Rahmahkh <125110979+Rahmahkh@users.noreply.github.com>
2024-07-14 23:48:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "<i>Innovative Electronics</i>",
+    "<i>Seamless Integration</i>",
+    "<i>Transformative Technology</i>",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
